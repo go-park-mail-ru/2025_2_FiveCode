1cdad5ad277a595371772ca9f2b45a2fcab9a27f
andrey57x <57andreys57@gmail.com>
2025-12-17 17:44:07 +0300

    registration validation fix

--- user_service/db/migrations/000001_init.up.sql
+++ user_service/db/migrations/000001_init.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "user"
 (
     id             INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email          TEXT        NOT NULL UNIQUE CHECK (LENGTH(email) <= 40 AND
-                                                      email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+                                                      email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]{2,}$'),
     password_hash  TEXT        NOT NULL,
     username       TEXT        NOT NULL CHECK (LENGTH(username) >= 1 AND LENGTH(username) <= 40 AND
                                                       username ~ '^[a-zA-Z0-9_]+$'),
